826298b3865bce98f45579be53d0a19ca7e01a9a
PushpaU <pushpanadhan215@gmail.com>
2024-08-07 20:28:14 +0530

    overview,air date and rating are done in Add details page

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -44,8 +44,104 @@ class DetailsScreen extends StatelessWidget{
                             style:GoogleFonts.belleza(
                                 fontSize:17,
                                 fontWeight:FontWeight.w600,),),
-                        background:Image.network('${Constants.imagePath}${series.backDropPath}')
+                        background:ClipRRect(
+                            borderRadius:const BorderRadius.only(
+                                bottomLeft:Radius.circular(24),
+                                bottomRight:Radius.circular(24),
+
+                            ),
+                            child:Image.network(
+                            '${Constants.imagePath}${series.backDropPath}',
+                        filterQuality:FilterQuality.high,
+                        fit:BoxFit.fill,
+                        )
+                        )
                     ),
+                ),
+                SliverToBoxAdapter(
+                    child:Padding(
+                        padding:const EdgeInsets.all(12),
+                        child:Column(
+                            children:[
+                                Text(
+                                    'Overview',
+                                    style:GoogleFonts.openSans(
+                                        fontSize:30,
+                                        fontWeight:FontWeight.w800,),
+
+                                ),
+                                const SizedBox(height:16),
+                                Text(
+                                    series.overview,
+                                    style:GoogleFonts.roboto(
+                                        fontSize:20,
+                                        fontWeight:FontWeight.w400,),
+                                        textAlign:TextAlign.justify,
+
+                                ),
+                                const SizedBox(height:16),
+                                SizedBox(
+                                    child:Row(
+                                        mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                                        children:[
+                                            Container(
+                                                padding:const EdgeInsets.all(8),
+                                                decoration:BoxDecoration(
+                                                    border:Border.all(color:Colors.grey),
+                                                    borderRadius:BorderRadius.circular(10),
+
+                                                ),
+                                                child:Row(children:[
+                                                    Text(
+                                                        'Air date:',
+                                                        style:GoogleFonts.roboto(
+                                                            fontSize:17,
+                                                            fontWeight:FontWeight.bold,),
+                                                    ),
+                                                    Text(
+                                                        series.firstAirDate,
+                                                        style:GoogleFonts.roboto(
+                                                            fontSize:17,
+                                                            fontWeight:FontWeight.bold,),
+                                                    )
+                                                ],
+                                                ),
+
+                                            ),
+                                            Container(
+                                                padding:const EdgeInsets.all(8),
+                                                decoration:BoxDecoration(
+                                                    border:Border.all(color:Colors.grey),
+                                                    borderRadius:BorderRadius.circular(10),
+
+                                                ),
+                                                child:Row(
+                                                    children:[
+                                                       Text(
+                                                        'Rating:',
+                                                        style:GoogleFonts.roboto(
+                                                            fontSize:17,
+                                                            fontWeight:FontWeight.bold,),
+                                                    ), 
+                                                    const Icon(
+                                                        Icons.star,
+                                                        color:Colors.amber,
+                                                    ),
+                                                    Text(
+                                                        '${series.voteAverage.toStringAsFixed(1)}/10',
+                                                        style:GoogleFonts.roboto(
+                                                            fontSize:17,
+                                                            fontWeight:FontWeight.bold,),
+                                                    )
+                                                    ]
+                                                )
+                                            )
+                                        ]
+                                    )
+                                ),
+                            ]
+                        )
+                    )
                 )
             ],),
         );
